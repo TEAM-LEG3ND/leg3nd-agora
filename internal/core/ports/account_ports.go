1790ed50415f0659204e717e25d83b6f062765f5
d0lim <limdongyoung0@gmail.com>
2023-03-26 18:28:40 +0900

    feat: add port interfaces

---
+++ internal/core/ports/account_ports.go
@@ -0,0 +1,16 @@
+package ports
+
+import (
+	"context"
+	"leg3nd-agora/internal/core/domain"
+)
+
+type AccountService interface {
+	CreateAccount(ctx context.Context, email string, nickname string, fullName string, oAuthProvider domain.OAuthProvider) (*domain.Account, error)
+	FindAccountById(ctx context.Context, id int64) (*domain.Account, error)
+}
+
+type AccountRepository interface {
+	Save(ctx context.Context, ac *domain.Account) (*domain.Account, error)
+	FindById(ctx context.Context, accountId int64) (*domain.Account, error)
+}
