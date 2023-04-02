453334551c5a7367756729899f4b55d5a3a2b2c8
d0lim <limdongyoung0@gmail.com>
2023-04-02 17:16:05 +0900

    feat: modify interfaces for find account by email

--- internal/core/ports/account_ports.go
+++ internal/core/ports/account_ports.go
@@ -8,9 +8,11 @@ import (
 type AccountService interface {
 	CreateAccount(ctx context.Context, email string, nickname string, fullName string, oAuthProvider domain.OAuthProvider) (*domain.Account, error)
 	FindAccountById(ctx context.Context, id int64) (*domain.Account, error)
+	FindAccountByEmail(ctx context.Context, email string) (*domain.Account, error)
 }
 
 type AccountRepository interface {
 	Save(ctx context.Context, ac *domain.Account) (*domain.Account, error)
 	FindById(ctx context.Context, accountId int64) (*domain.Account, error)
+	FindByEmail(ctx context.Context, email string) (*domain.Account, error)
 }
