7f06759a40516c12a189e299d652853f73411b34
d0lim <limdongyoung0@gmail.com>
2023-04-02 16:40:50 +0900

    feat: add condition for nickname-less request of create account

--- internal/core/service/account_service.go
+++ internal/core/service/account_service.go
@@ -3,8 +3,11 @@ package service
 import (
 	"context"
 	"fmt"
+	"github.com/google/uuid"
 	"leg3nd-agora/internal/core/domain"
 	"leg3nd-agora/internal/core/ports"
+	"log"
+	"math/rand"
 )
 
 type AccountService struct {
@@ -19,6 +22,10 @@ func ProvideAccountService(repository ports.AccountRepository) *AccountService {
 }
 
 func (s *AccountService) CreateAccount(ctx context.Context, email string, nickname string, fullName string, oAuthProvider domain.OAuthProvider) (*domain.Account, error) {
+	if nickname == "" {
+		nickname = getRandomNickname()
+	}
+	log.Println(nickname)
 	newAccount := domain.NewAccount(email, nickname, fullName, oAuthProvider)
 
 	savedAccount, err := s.repository.Save(ctx, newAccount)
@@ -37,4 +44,14 @@ func (s *AccountService) FindAccountById(ctx context.Context, id int64) (*domain
 	} else {
 		return acc, nil
 	}
+}
+
+func getRandomNickname() string {
+	colors := []string{"빨강", "주황", "노랑", "초록", "파랑", "보라", "누런", "갈색", "남색", "하늘", "하양", "검정", "까망", "누런"}
+	foods := []string{"마라샹궈", "알리오올리오", "김피탕", "짜장면", "짬뽕", "탕수육", "떡볶이", "양꼬치"}
+	uuidStr := uuid.New().String()
+
+	nickname := fmt.Sprintf("%s%s-%s", colors[rand.Intn(len(colors))], foods[rand.Intn(len(foods))], uuidStr)
+
+	return nickname
 }
