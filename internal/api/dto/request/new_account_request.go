2482c983fa66d6a7e2b65d2cf198bc37714cbd56
d0lim <limdongyoung0@gmail.com>
2023-04-23 18:39:06 +0900

    refactor: remove nickname from NewAccountRequest

--- internal/api/dto/request/new_account_request.go
+++ internal/api/dto/request/new_account_request.go
@@ -6,7 +6,6 @@ import (
 
 type NewAccountRequest struct {
 	Email         string               `json:"email"`
-	Nickname      string               `json:"nickname"`
 	FullName      string               `json:"full_name"`
 	OAuthProvider domain.OAuthProvider `json:"o_auth_provider"`
 }
